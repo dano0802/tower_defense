9dcde0142090c2d5b35704ccbda5bd2eb0cf6e02
dano0802 <dani.kekk@gmail.com>
2017-05-18 09:43:30 +0200

    Beolvaso kieg
    Kiegészítés kommentekkel

--- beolvaso_fgv.cpp
+++ beolvaso_fgv.cpp
@@ -1,26 +1,26 @@
 int beolvas(FILE *ptr_file, std::string *nev1, int *attr0, int *attr1, int *attr2, int *attr3, int *attr4)
 {
-    char buf[1000]; 
-    const char s[2] = ";"; 
-    char *token; 
+    char buf[1000]; //ebbe tesszük a beolvasott karakterláncot
+    const char s[2] = ";"; //elválasztó karakter
+    char *token; //részsztring eleje
     std::string nev;
     int attr[5];
     int i=-1;
               
-    if (fgets(buf,1000, ptr_file)!=NULL) 
+    if (fgets(buf,1000, ptr_file)!=NULL) //beolvasunk egy sort ha nem null jöhet a kinyerés
     {
-        token = strtok(buf, s); 
-		
-      
+        token = strtok(buf, s); //első token
+        
+        /* többi token */
         while( token != NULL ) 
         {
             if (i<0)
             {
-                nev = token; 
+                nev = token; //név sztring átadása
             }
             else
             {
-                attr[i] = atoi(token); 
+                attr[i] = atoi(token); //intre konvertálás
             }
             i++;
             token = strtok(NULL, s);
@@ -30,10 +30,10 @@ int beolvas(FILE *ptr_file, std::string *nev1, int *attr0, int *attr1, int *attr
     }
     else
     {
-        return -1; 
+        return -1; //ha vége a fájlnak
     }
     
-   
+    /*átadjuk a külső változóknak az értéket*/
     *nev1 = nev;
     *attr0=attr[0];
     *attr1=attr[1];
@@ -41,5 +41,5 @@ int beolvas(FILE *ptr_file, std::string *nev1, int *attr0, int *attr1, int *attr
     *attr3=attr[3];
     *attr4=attr[4];
     
-    return 0; 
+    return 0; //ha megtörtént az adatok kinyerése
 }
